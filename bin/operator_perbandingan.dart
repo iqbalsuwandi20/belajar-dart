1e00a4ca7ae40e56167c51861ab693dcf3cdbdd2
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-06 14:44:56 +0700

    cara membuat operator perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,13 @@
+void man() {
+
+  // Operator Perbandingan
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print("Iqbal" == "Iqbal");
+  print("Iqbal" != "Iqbal");
+}
