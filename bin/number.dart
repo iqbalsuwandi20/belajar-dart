65b6ac563ec6edddc03528b69b0a2933531d7ad0
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-06 14:01:53 +0700

    cara membuat number

---
+++ bin/number.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  // Tipe Data Number
+  int number1 = 10;
+  double number2 = 10.10;
+
+  print(number1);
+  print(number2);
+
+  // Num
+  num number = 10;
+  print(number);
+
+  number = 123.12;
+  print(number);
+}
