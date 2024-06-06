77c46fe829a6e5bbb6e4d9945f42f8b475d775f6
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-06 19:03:19 +0700

    cara membuat operator penugasan

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  // Operator Penugasan
+  var a = 10;
+
+  a += 10;
+  print(a);
+
+  // Increment dan Derement
+  var i = 0;
+  // i++ // i = i + 1;
+
+  var j = i++; // j = i, i++
+  var k = ++i; // k = (++1)
+
+  print(j);
+  print(k);
+}
