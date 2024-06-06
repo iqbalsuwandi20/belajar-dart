0ff5bf50068dbfca7711aa86fa69765c41e4d203
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-06 13:56:42 +0700

    komentar

---
+++ bin/komentar.dart
@@ -0,0 +1,12 @@
+/// Ini adalah komentar untuk dokumentasi
+/// will executed by dart
+void main() {
+  // Untuk satu baris
+
+  /**
+   * ini adalah
+   * komentar lebih
+   * dari 1 baris
+   * Iqbal Suwandi
+   */
+}
