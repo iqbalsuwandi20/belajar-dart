e36e4e4ba6691c4994bc5b4e3611f8cfc8fec3cc
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-06 14:40:22 +0700

    cara membuat operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  /**
+   * Operator Aritmatika
+   * ~/ pembagian hasil int
+   */
+  var first = 10;
+  var second = 2;
+
+  print(first + second);
+  print(first - second);
+  print(first * second);
+  print(first / second);
+  print(first ~/ second);
+  print(first & second);
+}
