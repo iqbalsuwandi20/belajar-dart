490e7a0f25b9e7c2ba0e41aae2e0862eb44f44fe
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-07 15:29:33 +0700

    cara membuat for in

---
+++ bin/for_in.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  var array = <String> ["Iqbal", "Suwandi", "Roger"];
+
+  for (var value in array) {
+    print(value);
+  }
+
+  var nameSet = <String>{"Iqbal", "Suwandi", "Roger"};
+
+  for (var element in nameSet) {
+    print(element);
+  }
+}
