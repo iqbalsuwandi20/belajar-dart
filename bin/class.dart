27e841047f9c0cb452cce101a19ffc1dc61be436
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-10 18:53:16 +0700

    cara membuat method

--- bin/class.dart
+++ bin/class.dart
@@ -5,6 +5,11 @@ class Person {
   String? address;
   final String country = "Indonesia";
 
+  /// Cara membuat Method di Dart
+  void sayHello(String paramName) {
+    print("Hello $paramName, My name is $name, I live in $address");
+  }
+
 }
 
 void main() {
@@ -17,12 +22,15 @@ void main() {
 
   /// Manipulasi Field
   person1.name = "Iqbal Suwandi";
-  person1.address = "Kota Tangerang";
+  person1.address = "Tangerang";
   
   /// Cara mengakses Field
   print(person1.name);
   print(person1.address);
   print(person1.country);
+
+  /// Cara mengakses Method
+  person1.sayHello("Khaleed");
 
   Person person2 = Person();
   print(person2);
