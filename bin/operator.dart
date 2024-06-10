f12d8253afb33a09e601c69120e9ce8828d90f32
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-10 19:19:35 +0700

    cara membuat operator

---
+++ bin/operator.dart
@@ -0,0 +1,26 @@
+class Orange {
+
+  /// Cara membuat Operator
+  int quantity = 0;
+
+  Orange operator +(Orange other) {
+    var result = Orange();
+    result.quantity = quantity + other.quantity;
+    return result;
+  }
+
+}
+
+void main() {
+
+  /// Cara mengakses Operator
+  var orange1 = Orange();
+  orange1.quantity = 10;
+
+  var orange2= Orange();
+  orange2.quantity = 10;
+
+  var orange3 = orange1 + orange2;
+  print(orange3.quantity);
+
+}
