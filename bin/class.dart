5b7445a1ece52c8aeaaa5b92ceeef3555a26dd6e
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-10 17:47:26 +0700

    cara membuat object

--- bin/class.dart
+++ bin/class.dart
@@ -1,3 +1,16 @@
 class Person {
-  
+
+}
+
+void main() {
+
+  /**
+   * Cara membuat Object di Dart
+   * Kata kunci New di Dart sudah tidak di rekomendasikan 
+   */
+  var person1 = Person();
+  print(person1);
+
+  Person person2 = Person();
+  print(person2);
 }
