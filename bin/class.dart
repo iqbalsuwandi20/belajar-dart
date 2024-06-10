e79894f8d9e7b50b70f549e49ddcb9dc6b4e8079
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-10 18:45:28 +0700

    cara membuat field

--- bin/class.dart
+++ bin/class.dart
@@ -1,5 +1,10 @@
 class Person {
 
+  /// Cara membuat Field di Dart 
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
+
 }
 
 void main() {
@@ -9,7 +14,15 @@ void main() {
    * Kata kunci New di Dart sudah tidak di rekomendasikan 
    */
   var person1 = Person();
-  print(person1);
+
+  /// Manipulasi Field
+  person1.name = "Iqbal Suwandi";
+  person1.address = "Kota Tangerang";
+  
+  /// Cara mengakses Field
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
 
   Person person2 = Person();
   print(person2);
