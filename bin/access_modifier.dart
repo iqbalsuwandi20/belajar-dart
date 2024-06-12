ac1135a75bc99a28810d4a4bb7b75e155c83f5db
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-12 16:33:02 +0700

    cara membuat access modifier

---
+++ bin/access_modifier.dart
@@ -0,0 +1,9 @@
+import 'data/product.dart';
+
+void main() {
+
+  Product product = Product();
+  product.id = "1";
+  product.name = "Laptop";
+  // product._quantity = 100; ERROR 
+}
---
+++ bin/data/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  
+  String? id;
+  String? name;
+  int? _quantity;
+
+  int? getQuantity() {
+    return _quantity;
+  }
+  
+}
