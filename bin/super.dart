e57f422da02ecd7abc18dd92a1ad67a5a19ee3a9
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-12 15:32:38 +0700

    cara membuat super keyword

---
+++ bin/super.dart
@@ -0,0 +1,27 @@
+class Shape {
+
+  int getCorner() {
+    return 0;
+  }
+
+}
+
+class Rectangle extends Shape {
+
+  @override
+  int getCorner() {
+    return 4;
+  }
+
+  int getParentCorner() {
+    return super.getCorner();
+  }
+
+}
+
+void main() {
+
+  Rectangle rectangle = Rectangle();
+  print(rectangle.getCorner());
+  print(rectangle.getParentCorner());
+}
