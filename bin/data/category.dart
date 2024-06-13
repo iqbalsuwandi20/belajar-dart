29846b5cb0f4de07fa256b9c5866125635a19d97
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-13 16:01:43 +0700

    cara membuat equals operator

--- bin/data/category.dart
+++ bin/data/category.dart
@@ -4,5 +4,21 @@ class Category {
   String name;
 
   Category(this.id, this.name);
+
+  bool operator ==(Object object) {
+
+    if (object is Category) {
+      if (id != object.id) {
+        return false;
+      } else if (name != object.name) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+    
+  }
   
 }
---
+++ bin/equals.dart
@@ -0,0 +1,10 @@
+import 'data/category.dart';
+
+void main() {
+
+  Category category1 = Category("1", "Laptop");
+  Category category2 = Category("1", "Laptop");
+
+  print(category1 == category1);
+  print(category1 == category2);
+}
