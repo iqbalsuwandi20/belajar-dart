2072b939ce50c284da3f80228232704a2f2f6466
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-14 16:46:34 +0700

    cara membuat static

---
+++ bin/static_field.dart
@@ -0,0 +1,13 @@
+class Application {
+
+  static final String name  ="Belajar Dart OOP";
+  static final String author = "Iqbal Suwandi";
+
+}
+
+void main() {
+
+  print(Application.name);
+  print(Application.author);
+
+}
---
+++ bin/static_method.dart
@@ -0,0 +1,11 @@
+class Math {
+
+  static int sum(int first, int second) => first + second;
+
+}
+
+void main() {
+
+  print(Math.sum(123132, 1312312));
+  
+}
