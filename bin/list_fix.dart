bb1ccd51d519d4312adf75cb7056715a851312f2
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-21 15:25:47 +0700

    cara membuat list

---
+++ bin/list_fix.dart
@@ -0,0 +1,14 @@
+void main() {
+
+  final list = List<int>.filled(10, 0);
+
+  print(list);
+
+  list[0] = 1;
+  list[1] = 12323;
+  list[2] = 3232;
+  list[3] = 88947589789;
+
+  print(list);
+  
+}
