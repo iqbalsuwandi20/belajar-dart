7791827accb44961d1cb9d862b1fb26143d44b85
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-21 16:36:38 +0700

    cara membuat linked list

---
+++ bin/linked_list.dart
@@ -0,0 +1,23 @@
+import 'dart:collection';
+
+class StringEntry extends LinkedListEntry<StringEntry> {
+
+  String value;
+
+  StringEntry(this.value);
+
+}
+
+void main() {
+
+  final linkedList = LinkedList<StringEntry>();
+
+  linkedList.add(StringEntry("Iqbal"));
+  linkedList.add(StringEntry("Suwandi"));
+  linkedList.add(StringEntry("Roger"));
+
+  for (var element in linkedList) {
+    print(element.value);
+  }
+  
+}
