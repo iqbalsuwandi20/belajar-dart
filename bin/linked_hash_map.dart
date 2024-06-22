4f4eb96162aa047df042fcae14bdf4d0af7c316c
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-22 16:55:20 +0700

    cara membuat linked hash map

---
+++ bin/linked_hash_map.dart
@@ -0,0 +1,20 @@
+import 'dart:collection';
+
+void main() {
+
+  // ignore: prefer_collection_literals
+  final scores = LinkedHashMap<String, int>();
+
+  scores["Iqbal"] = 100;
+  scores["Roger"] = 100;
+  scores["Gusion"] = 100;
+  scores["Khaleed"] = 100;
+  scores["Lancelot"] = 100;
+  scores["Thamuz"] = 100;
+  scores["Natan"] = 100;
+  scores["Xavier"] = 100;
+  scores["Pharsa"] = 100;
+
+  print(scores);
+  
+} 
