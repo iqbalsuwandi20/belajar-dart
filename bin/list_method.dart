a794e2e17f670e93aec3593fd3c382d240731c1c
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-22 16:31:55 +0700

    cara membuat list method

---
+++ bin/list_method.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  final names = ["Iqbal", "Suwandi"];
+
+  print(names);
+
+  names.insert(1, "Roger");
+
+  print(names);
+
+}
---
+++ bin/list_operator.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  final names = ["Iqbal", "Roger", "Suwandi"];
+  final authors = ["Programmer", "Zaman", "Now"];
+
+  final combine = names + authors;
+
+  print(names);
+  print(authors);
+  print(combine);
+  
+}
