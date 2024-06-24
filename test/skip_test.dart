603984b339c40df302d69a4ddccae7c52a090250
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-24 17:31:51 +0700

    cara membuat skip test

---
+++ test/skip_test.dart
@@ -0,0 +1,18 @@
+@Skip("This unit test still broker")
+
+import 'package:test/test.dart';
+
+int sum(int a, int b) => a + b;
+
+void main() {
+  
+  group("test sum()", (){
+    test("positive", (){
+      expect(sum(1, 2), equals(3));
+    });
+    test("negative", (){
+      expect(sum(10, -5), equals(5));
+    });
+  });
+
+}
