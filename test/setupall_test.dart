f4758299a613c4c0ef38fc844dcebfa1dbf9b6a4
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-24 16:48:46 +0700

    cara membuat setupall function

---
+++ test/setupall_test.dart
@@ -0,0 +1,33 @@
+import 'package:test/test.dart';
+
+void main() {
+
+  var data = "Iqbal";
+
+  setUpAll((){
+    print("Start Unit Test");
+  });
+
+  setUp((){
+    data = "Iqbal";
+    print(data);
+  });
+
+  tearDown((){
+    print(data);
+  });
+  
+  group("Test String", (){
+
+    test("String first", (){
+      data = "$data Suwandi";
+      expect(data, equals("Iqbal Suwandi"));
+    });
+
+    test("String second", (){
+      data = "$data Roger";
+      expect(data, equals("Iqbal Roger"));
+    });
+
+  });
+}
