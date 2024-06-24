ffebfeac0f7567539a1ff171a6c3f9d67d7eb989
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-24 18:01:36 +0700

    tag

---
+++ test/tag_test.dart
@@ -0,0 +1,14 @@
+@Tags(["PZN", "Iqbal"])
+
+import 'package:test/test.dart';
+
+void main() {
+
+  test("test first", (){
+    print("first");  
+  }, tags: ["first"]);
+
+  test("test second", (){
+    print("second");
+  }, tags: ["second"]);
+}
