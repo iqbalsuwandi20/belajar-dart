2d09d8565652d2732c3bff054bb4adaa91f9e846
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-26 13:59:39 +0700

    cara membuat stream

---
+++ bin/stream.dart
@@ -0,0 +1,13 @@
+Stream<String> stream() {
+  return Stream.periodic(Duration(seconds: 2), (i) {
+    if (i % 2 == 0) {
+      return "$i : Genap";
+    } else {
+      return "$i : Ganjil";
+    }
+  });
+}
+void main() {
+  stream();
+  print("Done");
+}
