923c8a3577cccee30ac886528044a78b78fcb69e
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-27 11:01:50 +0700

    stream subcription

--- bin/stream.dart
+++ bin/stream.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 Stream<String> stream() {
   return Stream.periodic(Duration(seconds: 2), (i) {
     if (i % 2 == 0) {
@@ -8,6 +10,21 @@ Stream<String> stream() {
   });
 }
 void main() {
-  stream();
+  Stream<String> flow = stream();
+  StreamSubscription<String> listen = flow.listen((event) {
+    print(event); // Tergantikan oleh onDone
+  });
+  listen.onData((data) {
+    print("Stream Subcription : $data");
+  });
+  listen.onDone(() {
+    print("Stream Subcription Done");
+  });
+
+  // tidak bisa di listen dua kali, error
+  // StreamSubscription<String> listen2 = flow.listen((event) {
+  //   print(event);
+  // });
+
   print("Done");
 }
