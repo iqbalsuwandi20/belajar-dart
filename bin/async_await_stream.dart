d533328490511828408121a92932b3a72bb96e74
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-27 12:44:51 +0700

    cara membuat async await stream

---
+++ bin/async_await_stream.dart
@@ -0,0 +1,22 @@
+Stream<String> names() {
+  return Stream.fromIterable(["Iqbal", "Suwandi", "Roger"]);
+}
+
+Future<String> fullName() async {
+
+  String name = "";
+  await for(String value in names()) {
+    name += "$value ";
+  }
+  return name.trim();
+
+}
+
+void main() {
+
+  fullName()
+    .then((value) => print(value));
+
+  print("Done");
+  
+}
