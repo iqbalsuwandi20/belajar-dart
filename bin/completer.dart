3aac13638e75c20e12d280cc45d93035e082c13c
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-28 09:35:38 +0700

    cara membuat completer

---
+++ bin/completer.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+void longRunningTask(void Function(String) onDone, void Function(Object?) onError) {
+
+  // Library orang lain
+  Future.delayed(Duration(seconds: 5))
+    .onError((error, stackTrace) => onError(error))
+    .then((value) => onDone("Task Completed"));
+    
+}
+
+Future<String> runLongRunningTask() {
+
+  Completer<String> completer = Completer();
+
+  longRunningTask((data) {
+    completer.complete(data);
+  }, (error) {
+    completer.completeError(error!);
+  });
+
+  return completer.future;
+
+}
+
+void main() {
+
+  runLongRunningTask()
+    .then((value) => print(value));
+
+  print("Done");
+
+}
