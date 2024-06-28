cd4555ade802cb0dffdb2dd779ad13deb49fab8d
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-28 10:05:18 +0700

    cara membuat generator

---
+++ bin/generator.dart
@@ -0,0 +1,15 @@
+Iterable<int> syncNumber() sync* {
+
+  for (var i = 0; i < 10; i++) {
+    yield i;
+  }
+
+}
+
+void main() {
+
+  syncNumber().forEach((element) {
+    print(element);
+  });
+  
+}
---
+++ bin/generator_async.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+Stream<int> asyncNumber() async* {
+
+  for (var i = 0; i < 10; i++) {
+    yield i;
+  }
+
+}
+
+void main() {
+
+  asyncNumber().listen((event) {
+    print(event);
+  });
+
+  print("Done");
+  
+}
