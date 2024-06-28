32a060760fc86ff59615a9a140a1930cefdd1217
Iqbal Suwandi <iqbalsuwandi20@gmail.com>
2024-06-28 09:05:13 +0700

    cara membuat isolate

---
+++ bin/isolate_stuck.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+Future<String> block() async {
+  
+  sleep(Duration(seconds: 2));
+  return "Block";
+
+}
+
+void main() {
+
+  print("Before");
+
+  block()
+    .then((value) => print(value));
+
+  print("Done");
+
+}
